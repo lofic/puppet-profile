f9f4b761d4423b5d17635fd0bcf32bc012f24c57
Louis Coilliot <louis.coilliot@gmail.com>
2021-02-26 11:44:47 +0100

    heredocs and colored less

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,44 +2,53 @@
 
 class profile {
 
-    $profileentries = [
-        'alias vi=vim',
-        "alias hc='rm -f ~/.bash_history; history -c'",
-        'HISTSIZE=1000',
-        'HISTFILESIZE=0',
-    ]
+    augeas { 'no beeps':
+        incl    => '/etc/inputrc',
+        lens    => 'inputrc.lns',
+        changes => [ 'set bell-style none' ]
+    }
+
+    $profile = @(PROFILE/L)
+        alias vi=vim
+        alias hc='rm -f ~/.bash_history; history -c'
+        HISTSIZE=1000
+        HISTFILESIZE=0
+        | PROFILE
 
     file { '/etc/profile.d/custom.sh' :
         ensure  => present,
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        content => inline_template(
-            "<% @profileentries.each do |pe| -%><%= pe %>\n<% end -%>"
-        )
+        content => $profile,
     }
 
-    $pupaliases = [
-        'alias pat=\'if [ $UID -eq 0 ]; then puppet agent --test; fi\'',
-        'alias pcl=\'if [ $UID -eq 0 ]; then cat $(puppet agent --configprint classfile); fi\'',
-        'alias prl=\'if [ $UID -eq 0 ]; then cat $(puppet agent --configprint resourcefile); fi\'',
-        "alias pae='sudo -i puppet agent --configprint environment'",
-    ]
+    $pupaliases = @(PUPALIASES/L)
+        alias pat='if [ $UID -eq 0 ]; then puppet agent --test; fi'
+        alias pcl='if [ $UID -eq 0 ]; then cat $(puppet agent --configprint classfile); fi'
+        alias prl='if [ $UID -eq 0 ]; then cat $(puppet agent --configprint resourcefile); fi'
+        alias pae='sudo -i puppet agent --configprint environment'
+        | PUPALIASES
 
     file { '/etc/profile.d/custom-puppet.sh':
         ensure  => present,
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        content => inline_template(
-            "<% @pupaliases.each do |pa| -%><%= pa %>\n<% end -%>"
-        )
+        content => $pupaliases,
     }
 
-    augeas { 'no beeps':
-        incl    => '/etc/inputrc',
-        lens    => 'inputrc.lns',
-        changes => [ 'set bell-style none' ]
+    $coloredless = @(COLOREDLESS/L)
+        export LESSOPEN="| /usr/bin/source-highlight-esc.sh %s"
+        export LESS='-R '
+        | COLOREDLESS
+
+    file { '/etc/profile.d/custom-colors.sh':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => $coloredless,
     }
 
 }
